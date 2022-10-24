0559785fccdb51dc88a4e7ca6bc50dd92ed84e2f
Aditya L <aditya.net09@gmail.com>
2022-10-23 23:19:33 -0700

    added query, new database creation functionality

--- db.py
+++ db.py
@@ -2,14 +2,28 @@ import mysql.connector
 from mysql.connector import Error
 import pandas as pd
 
+#Queries
+#TODO Add queries as strings to be called
+CREATE_PASS_TABLE = '''
+CREATE TABLE securePassword ()
+'''
+
+# Query for adding a new password, 
+# Reqd: service, key, tag, encrypted pass, nonce
+
+ADD_PASSWORD = '''
+INSERT INTO securePassword VALUES()
+'''
+
 #function to connect to the db
 #params: host name, username, db password
 
-# params: host name, user name and password
+# params: host name, user name and password (database name)
 # returns: sql connection object if connection succesful
 #           or error if unsuccessful 
 # connection = none to close any previous connections
 
+#for first time connection, check if db exists, else
 def create_connection(host, user, password):
     connection = None
     try:
@@ -17,6 +31,9 @@ def create_connection(host, user, password):
             host = host,
             user = user,
             passwd = password 
+            #database = db_name
+            #argument if database already exists
+
         )
         print("connection successful")
     except Error as err:
@@ -26,4 +43,21 @@ def create_connection(host, user, password):
 
 
 def new_database(connection, query):
-    cursor = connection.cursor()  
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        print("database created")
+    except Error as er:
+        print(f"Error: {er}")  
+
+########################################################
+
+def execute_query(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("query successful")
+    except Error as er:
+        print(f"error: {er}")
+
